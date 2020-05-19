e1b701b4831e50d92b0ea1255cc371cef7032014
alamalam <alamcahya3211@gmail.com>
2020-05-19 15:51:28 +0700

    fixing category add service

--- src/domain/repositories/ICategoryRepository.ts
+++ src/domain/repositories/ICategoryRepository.ts
@@ -1,6 +1,7 @@
 import Category from '../models/Category';
+import { CategoryRequest } from '../../application/category/CategoryRequest';
 
 export interface ICategoryRepository {
-  add(category: Category): Promise<Category>;
+  add(category: CategoryRequest): Promise<Category>;
   getAll(): Promise<Category>;
 }
