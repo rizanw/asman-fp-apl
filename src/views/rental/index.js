0ce1c7c2ebee23b229737b6e0d3c4a5b44a0e584
rizanw <rzkandre@gmail.com>
2020-05-31 23:05:57 +0700

    status label

--- src/views/rental/index.js
+++ src/views/rental/index.js
@@ -6,6 +6,10 @@ import RentInContainer from "./RentIn.container";
 import MyRentalContainer from "./MyRental.container";
 import RentOutContainer from "./RentOut.container";
 
+// View Rental ini belum sepenuhnya selesai.. 
+// TO-DO: 
+// - create & update
+
 export default class Index extends React.Component {
     render() {
         return (
